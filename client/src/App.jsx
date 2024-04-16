1d4e59b99527e3774e04d07eb8f58cdba756a224
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-04-16 13:56:46 +0530

    second commit"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold  text-3xl text-red-300 underline">
+      this is second commit
+    </h1>
+  );
 }
