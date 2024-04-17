8e88770b11c94672e075e711d5be5caa534497e3
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-04-17 05:58:49 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -19,3 +21,5 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use("/api/user", userRoutes);
